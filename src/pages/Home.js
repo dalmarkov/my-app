ed7ba094c42fdfe56e782f9b604021fed4d0c376
Anastasiya Karbalevich <anastasiyakarbalevich@admins-MacBook-Air.local>
2024-05-24 15:36:49 +0200

    refactor style

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,5 @@
 import Header from "./../components/header/Header"
+import "./stylesHome.css"
 
 const Home = () => {
     return (
@@ -6,17 +7,35 @@ const Home = () => {
           <Header />
           <main className="section">
           <div className="container">
-                <ul className="content-list">
-                    <li className="content-list__item">
-                        <h2 className="title-2">Frontend</h2>
-                        <p>JavaScript, TypeScript, ReactJS, Redux, HTML, CSS, NPM, BootStrap, Yarn, TailwindCSS, StyledComponents</p>
+                <ul className="content-list-all">
+                    <li className="content-list__skills">
+                    <h2 className="my-skills">DEVELOPMENT</h2>
+                        <div className="skills-container">
+                             <div className="skills">JavaScript</div> 
+                             <div className="skills">TypeScript</div>
+                             <div className="skills">HTML</div>
+                            <div className="skills">CSS</div>
+                        </div>
                     </li>
-                    <li className="content-list__item">
-                        <h2 className="title-2">Backend</h2>
-                        <p>NodeJS</p>
+                    <li className="content-list__skills">
+                    <h2 className="my-skills">LIBRARY & FRAMEWORK</h2>
+                    <div className="skills-container">
+                        <div className="skills">ReactJS</div>
+                        <div className="skills">Redux</div>
+                        <div className="skills">BootStrap</div>
+                        <div className="skills">TailwindCSS</div>
+                        <div className="skills">Yarn</div>
+                        <div className="skills">Npm</div>
+                        <div className="skills">StyledComponents</div>
+                    </div>
+                    </li>
+                    <li className="content-list__skills">
+                    <h2 className="my-skills">BACKEND</h2>
+                    <div className="skills-container">
+                        <div className="skills">NodeJS</div>
+                    </div>
                     </li>
                 </ul>
-
           </div>
           </main>
         </>
