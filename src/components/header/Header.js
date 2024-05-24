85cf6a9fec11d32ef78c88d601734cf9a183efa5
Anastasiya Karbalevich <anastasiyakarbalevich@admins-MacBook-Air.local>
2024-05-24 13:23:44 +0200

    edit style

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -6,15 +6,12 @@ const Header = () => {
         <header className="header">
         <div className="header__wrapper">
             <h1 className="header__title">
-                <strong>Hi, my name is <em>Dima</em></strong><br/>
-                a 
-                frontend developer 
-
+                <strong>HI, I'M <em>DIMA</em><br/>
+                A 
+                FRONTEND DEVELOPER <br/> 
+                WITH PASSION <br/>FOR CREATING.
+                </strong>
             </h1>
-            <div className="header__text">
-                <p>with passion for learning and creating.</p>
-            </div>
-            <a href="#!" className="btn">Download CV</a>
         </div>
     </header>
 
